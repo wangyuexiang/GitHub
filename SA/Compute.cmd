1c0ab37e243592bbb9a985d9fdc5aefd05d9d0c5
wangyuexiang <yuexiang.wang@hotmail.com>
2015-12-22 14:22:24 +0100

    Add R & R Pkgs

---
+++ SA/Compute.cmd
@@ -0,0 +1,3 @@
+@echo Debut script : %date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
+@rscript Algo.R
+@echo Fin script : %date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
